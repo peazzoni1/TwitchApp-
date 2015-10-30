fdc9fb638549caebe403c0ba30dc3613eee1e577
peazzoni1 <peazzoni@yahoo.com>
2015-10-30 15:08:03 -0400

    added functions to get online or offline status and icons

--- app.js
+++ app.js
@@ -2,12 +2,27 @@ var app = angular.module('twitchApp', []);
 
 app.controller('UserCtrl', ['$http', function($http) {
  var vm = this; 
+//initialize empty array for table 
  vm.userData = [];
-  
+  //make API call and push json data to array
   vm.getUser = function() {
-  $http.get('https://api.twitch.tv/kraken/channels/' + vm.newUser).success(function(data) {
-    vm.userData.push({"game" : data.game, "name" : data.display_name, "logo" : data.logo});
-    });
+  $http.get('https://api.twitch.tv/kraken/streams/' + vm.newUser).success(function(data) {
+      if (data.stream !== null) {  
+    
+vm.userData.push({"game" : data.stream.game, "name" :    data.stream.channel.display_name, "logo" : data.stream.channel.logo, "status" : "fa fa-check"});
+    
     vm.newUser = ' ';
-  }
+}
+  else { 
+      
+      $http.get(data._links.channel).success(function(data) { 
+      vm.userData.push({"game" : data.game, "name" : data.name, "logo" : data.logo, "status" : "fa fa-exclamation" });
+      });
+       
+      vm.newUser = ' ';
+      } 
+  });
+  }                                                                         
 }]);
+      
+      
